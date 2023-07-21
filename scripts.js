2fdc00b69002110460b88d35cc385fae9ca105b0
JJ Ortiz <12117039+jj19@users.noreply.github.com>
2023-07-20 17:18:41 -0700

    add edit function version 1

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ class taskItem {
 		// Boolean - if item is completed (default: false)
 		this.completed = completed;
 		// Boolean - if item was marked as important (default: false)
-		this.important = important; 
+		this.important = important;
 	}
 
 	getContent() {
@@ -39,83 +39,89 @@ function addItem(click) {
 	let todoList = document.querySelector('#todoList');
 	let todoInput = document.querySelector('#todoInput').value;
 	let newTodo = document.createElement('div');
-	newTodo.setAttribute("id", `${Date.now()}`);
+	newTodo.setAttribute('id', `${Date.now()}`);
 	// Allows focus/blur CCS styling
-	newTodo.setAttribute("tabindex", "0");
+	newTodo.setAttribute('tabindex', '0');
 
-
-	if (todoInput === "") {
-			alert('Please add a Task!');
+	if (todoInput === '') {
+		alert('Please add a Task!');
 	} else {
 		let newTask = new taskItem(todoInput, false, isImportantTodo.checked);
 		todoArray.push(newTask);
 		newTodo.className = 'todoItem';
 		if (isImportantTodo.checked) {
-			newTodo.classList.add("important")
+			newTodo.classList.add('important');
 		}
-		newTodo.innerHTML = 
-		`<div>
+		newTodo.innerHTML = `<div>
 			<p class="task-content">${newTask.getContent()}</p>
 		</div>
 		<div class="button-group">
-		</div>`
+		</div>`;
 		todoList.appendChild(newTodo);
 
 		// Remove/complete/edit buttons are created, with a different event listener applied to each
 
-		let removeTodo = document.createElement("button");
-		removeTodo.className = "btn btn-danger";
-		removeTodo.innerText = "Remove";
-		removeTodo.addEventListener("click", removeItem);
+		let removeTodo = document.createElement('button');
+		removeTodo.className = 'btn btn-danger';
+		removeTodo.innerText = 'Remove';
+		removeTodo.addEventListener('click', removeItem);
 
+		let completeTodo = document.createElement('button');
+		completeTodo.className = 'btn btn-success';
+		completeTodo.innerText = 'Complete';
+		completeTodo.addEventListener('click', markComplete);
 
-
-		let completeTodo = document.createElement("button");
-		completeTodo.className = "btn btn-success";
-		completeTodo.innerText = "Complete";
-		completeTodo.addEventListener("click", markComplete);
-		
-
-
-		let editTodo = document.createElement("button");
-		editTodo.className = "btn btn-dark";
-		editTodo.innerText = "Edit";
-		editTodo.addEventListener("click", editContent);
+		let editTodo = document.createElement('button');
+		editTodo.className = 'btn btn-dark';
+		editTodo.innerText = 'Edit';
+		editTodo.addEventListener('click', editContent);
 
 		newTodo.children[1].appendChild(removeTodo);
 		newTodo.children[1].appendChild(completeTodo);
 		newTodo.children[1].appendChild(editTodo);
-		
 
 		// Resets text box
-	 	document.querySelector('#todoInput').value = '';
-		}
-	 }
-
+		document.querySelector('#todoInput').value = '';
+	}
+}
 
 function removeItem(click) {
 	click.target.parentNode.parentNode.remove();
 }
 
 function markComplete(click) {
-	let badge = document.createElement("span");
-	badge.className = "badge bg-success-subtle text-success-emphasis rounded-pill";
-	badge.innerText = "Completed";
+	let badge = document.createElement('span');
+	badge.className =
+		'badge bg-success-subtle text-success-emphasis rounded-pill';
+	badge.innerText = 'Completed';
 	click.target.parentNode.previousElementSibling.appendChild(badge);
-
 }
 
 function editContent(click) {
 	// This should probably be a variable. It points to the task-content element.
-	console.log(click.target.parentNode.previousElementSibling.children[0]);
-	let newContent = document.createElement("input");
-	newContent.setAttribute("type", "text");
-	newContent.setAttribute("value", click.target.parentNode.previousElementSibling.children[0].innerText);
-	click.target.parentNode.parentNode.appendChild(newContent);
+	let taskContent = click.target.parentNode.previousElementSibling.children[0];
+
+	let newContent = document.createElement('div');
+	newContent.innerHTML = `<form id="editTask">
+  <input type="text" value= ${taskContent.innerText}></form>
+  `;
+	taskContent.parentNode.replaceChild(newContent, taskContent);
+	document.querySelector('#editTask').addEventListener('submit', editTask);
 
 	/* Further progress for this function:
-		- hiding the remove/complete/edit button group to disable multiple text elements
-		- additional buttons, also with event listeners, to confirm or cancel the edit
-		- inserting the text box between its siblings and then removing the task-content element
+  - hiding the remove/complete/edit button group to disable multiple text elements
+  - additional buttons, also with event listeners, to confirm or cancel the edit
+  - inserting the text box between its siblings and then removing the task-content element
 	*/
+}
+function editTask(e) {
+	e.preventDefault();
+	let modifiedTaskContent = e.target.children[0].value;
+	let modifiedTask = document.createElement('div');
+	modifiedTask.innerHTML = `<div>
+  <p class="task-content">${modifiedTaskContent}</p>
+</div>`;
+	modifiedTask.replaceChild(modifiedTask, modifiedTaskContent);
+
+	// let taskContent = e.target.parentNode.previousElementSibling.children[0];
 }
